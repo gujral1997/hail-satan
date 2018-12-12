a31e1e3a7a13ae7e8f68b5f8af26cf795ed172b5
Ansh Gujral <anshgujral1997@gmail.com>
2018-12-13 01:23:52 +0530

    created functionality to create new transactions
    Signed-off-by: Ansh Gujral <anshgujral1997@gmail.com>

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,26 +1,44 @@
 function Blocakchain() {
     this.chain = []
-    this.newTransactions = []
+    this.pendingTransactions = []
 }
+
+// To mine a new block
 
 Blocakchain.prototype.createNewBlock = function (nonce, previousBlockHash, hash) {
     const newBlock = {
         index: this.chain.length + 1,
         timestamp: Date.now(),
-        transactions: this.newTransactions,
+        transactions: this.pendingTransactions,
         nonce,
         hash,
         previousBlockHash
     }
 
-    this.newTransactions = []
+    this.pendingTransactions = []
     this.chain.push(newBlock)
 
     return newBlock
 }
 
+// All the pending transactions before mining
+
+Blocakchain.prototype.createNewTransaction = function(amount, sender, recipient) {
+    const newTransaction = {
+        amount,
+        sender,
+        recipient
+    }
+
+    this.pendingTransactions.push(newTransaction)
+
+    return this.getLastBlock()['index'] + 1
+}
+
+// To get info about last block
+
 Blocakchain.prototype.getLastBlock = function() {
     return this.chain[this.chain.length - 1]
 }
 
-// module.exports = Blocakchain
+module.exports = Blocakchain
