ef24c56d2be400adb530d1703e287967bb9d98a2
Ansh Gujral <anshgujral1997@gmail.com>
2018-12-14 22:56:10 +0530

    tested create new transaction functionality
    Signed-off-by: Ansh Gujral <anshgujral1997@gmail.com>

--- dev/test.js
+++ dev/test.js
@@ -3,7 +3,9 @@ const Blockchain = require('./blockchain')
 const bitcoin = new Blockchain()
 
 bitcoin.createNewBlock( 123, 'OIN', 'aa')
-bitcoin.createNewBlock( 1232, 'rwe', 'aaq')
-bitcoin.createNewBlock( 12323, 'OewIN', 'aaweq')
+bitcoin.createNewTransaction( 1232, 'rwe', 'aaq')
+bitcoin.createNewTransaction( 1232, 'rwe', 'aaq')
+bitcoin.createNewTransaction( 1232, 'rwae', 'aaaq')
+bitcoin.createNewBlock( 123, 'OIN', 'aa')
 
 console.log(bitcoin)
