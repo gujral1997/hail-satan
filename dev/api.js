6b1bf56ca3b5e18edcaf46de5386a2a44a646e61
Ansh Gujral <anshgujral1997@gmail.com>
2018-12-16 20:53:31 +0530

    added body parser, empty apis and nodemon configuration
    Signed-off-by: Ansh Gujral <anshgujral1997@gmail.com>

--- dev/api.js
+++ dev/api.js
@@ -1,8 +1,21 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 
-app.get('/', (req, res)=> {
-    res.send('Hello World')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get('/blockchain', (req, res)=> {
+    
+})
+
+app.post('/transaction', (req, res)=> {
+    console.log(req.body)
+    res.send('It works')
+})
+
+app.get('/mine', (req, res)=> {
+
 })
 
 app.listen(3000, ()=>console.log('listening on 3000'))
