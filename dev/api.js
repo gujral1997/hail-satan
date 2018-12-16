812b51c9c1e6c1c4577b45eccdecbdc8fe91b66e
Ansh Gujral <anshgujral1997@gmail.com>
2018-12-17 00:21:21 +0530

    created add transaction and get chain api
    Signed-off-by: Ansh Gujral <anshgujral1997@gmail.com>

--- dev/api.js
+++ dev/api.js
@@ -1,17 +1,20 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const Blockchain = require('./blockchain')
+
+const bitcoin = new Blockchain()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/blockchain', (req, res)=> {
-    
+    res.send(bitcoin)
 })
 
 app.post('/transaction', (req, res)=> {
-    console.log(req.body)
-    res.send('It works')
+    const blockIndex = bitcoin.createNewTransaction(req.body.amount, req.body.sender, req.body.recipient)
+    res.json({ note: `Transaction will be added in block ${blockIndex}` })
 })
 
 app.get('/mine', (req, res)=> {
