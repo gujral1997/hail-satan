b4c3621953bd9e7eb2ed6a2aaf1c57c00755300c
Ansh Gujral <anshgujral1997@gmail.com>
2018-12-17 01:06:36 +0530

    created mine coin api
    Signed-off-by: Ansh Gujral <anshgujral1997@gmail.com>

--- dev/api.js
+++ dev/api.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const Blockchain = require('./blockchain')
+const uuid = require('uuid/v1')
+
+const nodeAddress = uuid().split('-').join('')
 
 const bitcoin = new Blockchain()
 
@@ -18,7 +21,22 @@ app.post('/transaction', (req, res)=> {
 })
 
 app.get('/mine', (req, res)=> {
+    const lastBlock = bitcoin.getLastBlock()
+    const previousBlockHash = lastBlock['hash']
+    const currentBlockData = {
+        transactions: bitcoin.pendingTransactions,
+        index: lastBlock['index'] + 1
+    }
+    const nonce = bitcoin.proofOfWork(previousBlockHash, currentBlockData)
+    const blockHash = bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce)
 
+    bitcoin.createNewTransaction(10, '00', nodeAddress)
+
+    const newBlock = bitcoin.createNewBlock(nonce, previousBlockHash, blockHash)
+
+    res.json({
+        msg: "New Block mined successfully",
+        newBlock})
 })
 
 app.listen(3000, ()=>console.log('listening on 3000'))
