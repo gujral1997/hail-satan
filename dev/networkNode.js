c31dc686b01831bb26978ea26798ac6e221b0a5f
Ansh Gujral <anshgujral1997@gmail.com>
2018-12-17 15:34:50 +0530

    built post register and brodcast end point
    Signed-off-by: Ansh Gujral <anshgujral1997@gmail.com>

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser')
 const Blockchain = require('./blockchain')
 const uuid = require('uuid/v1')
 const port = process.argv[2]
+const rp = require('request-promise')
 
 const nodeAddress = uuid().split('-').join('')
 
@@ -38,6 +39,43 @@ app.get('/mine', (req, res)=> {
     res.json({
         msg: "New Block mined successfully",
         newBlock})
+})
+
+// registers a node and brodcasts it to the whole network
+app.post('/register-and-brodcast-node', (req, res)=> { 
+    const newNodeUrl = req.body.newNodeUrl
+    if (bitcoin.networkNodes.indexOf(newNodeUrl) == -1)
+        bitcoin.networkNodes.push(newNodeUrl)
+
+    const regNodesPromises = []
+
+    // Todo: brodcasting it to the other newtworks
+    bitcoin.networkNodes.forEach(networkNodeUrl => {
+        // register node
+        const requestOptions = {
+            uri: networkNodeUrl + '/register-node',
+            method: post,
+            body: { newNodeUrl },
+            json: true
+        }
+
+        regNodesPromises.push(rp(requestOptions))
+    })
+
+    Promise.all(regNodesPromises)
+    .then(data=> {
+        // use the data
+    })
+})
+
+// register a node with the network
+app.post('/register-node', (req, res)=> {
+
+})
+
+// register multiple nodes at once
+app.post('/register-nodes-bulk', (req, res)=> {
+
 })
 
 app.listen(port, ()=>console.log(`Listening on ${port}`))
